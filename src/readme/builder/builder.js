ae1665a4e0e5c028ccfdc02851bbe28fe8b861aa
imrok le bleu <romaric.ruga@gmail.com>
2019-06-17 15:13:39 +0200

    precise readme builder rejection

--- src/readme/builder/builder.js
+++ src/readme/builder/builder.js
@@ -3,10 +3,15 @@ const fs = require('fs')
 module.exports = path =>
   new Promise((resolve, reject) => {
     fs.readdir(path, (err, files) => {
+      console.log(files)
       if (err) {
         if (err.code === 'ENOENT') {
-          reject(err)
+          reject(new Error('target folder not found'))
         }
+        reject(err)
+      }
+      if (!files || files.length === 0 || !files.includes('index.json')) {
+        reject(new Error('no index.json file'))
       }
       resolve(true)
     })
--- src/readme/builder/builder.test.js
+++ src/readme/builder/builder.test.js
@@ -9,19 +9,22 @@ chai.should()
 
 describe('readme builder', () => {
 
-  it('TEMP // should resolve if provided path match folder', done => {
-    mock(_mocks['folder exists'])
-    const promise = builder('tmp')
-    promise.should.be.fulfilled
-    mock.restore()
-    done()
-  })
+  describe('target folder', () => {
 
-  it('should reject if provided path doesn\'t match any folder', done => {
-    mock(_mocks['no folder'])
-    const promise = builder('tmp')
-    promise.should.be.rejected
-    mock.restore()
-    done()
+    it('should reject if provided path doesn\'t match any folder', done => {
+      mock(_mocks['no folder'])
+      const promise = builder('test')
+      promise.should.be.rejectedWith('target folder not found')
+      mock.restore()
+      done()
+    })
+
+    it('should reject if folder doesn\'t contain an `index.json` file', done => {
+      mock(_mocks['folder exists'])
+      const promise = builder('test')
+      promise.should.be.rejectedWith('no index.json file')
+      mock.restore()
+      done()
+    })
   })
 })
