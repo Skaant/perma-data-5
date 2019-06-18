0b250ad39a10753cbba70e02189a1dc3cf23f116
imrok le bleu <romaric.ruga@gmail.com>
2019-06-18 11:40:23 +0200

    finished readme builder readIndex tests

--- src/readme/builder/readIndex/_mocks/index.js
+++ src/readme/builder/readIndex/_mocks/index.js
@@ -1,11 +1,9 @@
-const noIndex = require('./noIndex/noIndex')
-const notJson = require('./notJson/notJson')
-const notObject = require('./notObject/notObject')
-const ok = require('./ok/ok')
-
 module.exports = {
-  'no index': noIndex,
-  'not json': notJson,
-  'not object': notObject,
-  'ok': ok
+  'no file': require('./noFile/noFile'),
+
+  'format not json': require('./formatNotJson/formatNotJson'),
+  'format not object': require('./formatNotObject/formatNotObject'),
+
+  'content missing': require('./contentMissing/contentMissing'),
+  'content not array': require('./contentNotArray/contentNotArray'),
 }
