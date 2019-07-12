64037b4bb12b93a42058c8ad28d6efdda4f73acb
ROMARIC RUGA <ruga0001@ad-urca.univ-reims.fr>
2019-07-12 09:05:53 +0200

    middleware test + moved wrong call from langRouter

--- src/functions/apps/content/middlewares/lang/lang.js
+++ src/functions/apps/content/middlewares/lang/lang.js
@@ -1,5 +1,11 @@
+const langs = require('../../../../../../config.json').langs
+
 module.exports = (req, res, next) => {
   const lang = req.path.split('/')[1]
-  req.lang = lang
-  next()
+  if (!langs.includes(lang)) {
+    res.status(400).send('<html id="error" lang="en">kok</html>')
+  } else {
+    req.lang = lang
+    next()
+  }
 }
