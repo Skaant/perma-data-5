fa50b1038fd6b74e289859ebe86580dcfd6767fc
imrok le bleu <romaric.ruga@gmail.com>
2019-08-22 08:44:58 +0200

    serve a bundle

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
 const authMiddleware = require('./middlewares/authMiddleware/authMiddleware')
 
 const app = express()
+
+app.use('/public', express.static('public'))
 
 app.use(authMiddleware)
 app.use('/info', require('./routes/infoRouter/infoRouter'))
---
+++ src/bundles/modules/login/index.js
@@ -0,0 +1,6 @@
+export default
+  () => {
+    const login = 'login'
+
+    console.log(login)
+  }
