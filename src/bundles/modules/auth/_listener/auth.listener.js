b08984be6a42474e35110ff5ef476fa3cf8837f2
imrok le bleu <romaric.ruga@gmail.com>
2019-11-28 17:44:28 +0100

    fixed auth intialization

--- src/bundles/modules/auth/_listener/auth.listener.js
+++ src/bundles/modules/auth/_listener/auth.listener.js
@@ -2,56 +2,53 @@ import renderComponent from './renderComponent/renderComponent'
 import initialTokenCheck from './initialTokenCheck/initialTokenCheck'
 import setBackdropClickClose from '../../_utils/setBackdropClickClose/setBackdropClickClose'
 import loadBundle from './loadBundle/loadBundle'
-import { AUTH_LOGIN_MODAL_CLOSE } from '../_actions/auth.actions';
+import { AUTH_LOGIN_MODAL_CLOSE } from '../_actions/auth.actions'
+
+let previous = null
 
 export default () => {
 
-    // Compare logic
-    let previous = null
+  const next = window.__STORE__
+    .getState()
+    .auth
 
-    const store = window.__STORE__
+  if (next
+    && !next.tokenInitialCheck) {
 
-    store
-      .subscribe(() => {
-
-        const next = store
-          .getState()
-          .auth
-
-        if (previous
-            && !previous.user
-            && next.user
-            && !next.moduleLoaded) {
-
-          previous = next
-         
-          loadBundle(window.PAGE_ID)
-        }
-
-        renderComponent()
-
-        if (previous
-            && previous.modalDisplay !== next.modalDisplay) {
-         
-          if (next.modalDisplay === true) {
-            
-            $('#login-modal')
-              .modal('show')
-
-            setBackdropClickClose(
-              '#login-modal',
-              AUTH_LOGIN_MODAL_CLOSE
-            )
-
-          } else {
-
-            $('#login-modal')
-              .modal('hide')
-          }
-        }
-
-        previous = next
-      })
-          
     initialTokenCheck()
+    }
+
+  if (previous
+      && !previous.user
+      && next.user
+      && !next.moduleLoaded) {
+
+    previous = next
+    
+    loadBundle(window.PAGE_ID)
   }
+
+  renderComponent()
+
+  if (previous
+      && previous.modalDisplay !== next.modalDisplay) {
+    
+    if (next.modalDisplay === true) {
+      
+      $('#login-modal')
+        .modal('show')
+
+      setBackdropClickClose(
+        '#login-modal',
+        AUTH_LOGIN_MODAL_CLOSE
+      )
+
+    } else {
+
+      $('#login-modal')
+        .modal('hide')
+    }
+  }
+
+  previous = next
+}
