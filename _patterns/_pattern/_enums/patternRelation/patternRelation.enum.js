f6c1df2be4a84fe3b09d4134b5ebe90cf08e3df0
imrok le bleu <romaric.ruga@gmail.com>
2020-01-18 15:11:29 +0100

    chain description

--- _patterns/_pattern/_enums/patternRelation/patternRelation.enum.js
+++ _patterns/_pattern/_enums/patternRelation/patternRelation.enum.js
@@ -8,27 +8,43 @@ module.exports = {
    */
   PATTERN_RELATION_SPECIALIZATION: 'PATTERN_RELATION_SPECIALIZATION',
   /**
-   * This pattern is the aggregation source [parent].
-   * 
-   * *Target can exist independently of the pattern.*
-   */
-  PATTERN_RELATION_AGGREGATION_SOURCE: 'PATTERN_RELATION_AGGREGATION_SOURCE',
-  /**
    * This pattern is the composition source [parent].
    * 
    * *Target cannot exist independently of the pattern.*
    */
   PATTERN_RELATION_COMPOSITION_SOURCE: 'PATTERN_RELATION_COMPOSITION_SOURCE',
   /**
-   * This pattern is the aggregation target [children].
+   * This pattern is :
+   *  1. at the top, composition source [parent],
+   *  2. at the bottom, abstracting the target [shared].
    * 
-   * *Pattern can exist independently of the pattern.*
+   * *Target cannot exist independently of the pattern.*
    */
-  PATTERN_RELATION_AGGREGATION_TARGET: 'PATTERN_RELATION_AGGREGATION_TARGET',
+  PATTERN_RELATION_COMPOSITION_SPECIALIZATION_SOURCE: 'PATTERN_RELATION_COMPOSITION_SPECIALIZATION_SOURCE',
   /**
    * This pattern is the composition target [children].
    * 
    * *Pattern cannot exist independently of the pattern.*
    */
   PATTERN_RELATION_COMPOSITION_TARGET: 'PATTERN_RELATION_COMPOSITION_TARGET',
+  /**
+   * This pattern is :
+   *  1. at the bottom, composition target [children],
+   *  2. at the top, extending the source [shared].
+   * 
+   * *Pattern cannot exist independently of the pattern.*
+   */
+  PATTERN_RELATION_COMPOSITION_SPECIALIZATION_TARGET: 'PATTERN_RELATION_COMPOSITION_SPECIALIZATION_TARGET',
+  /**
+   * This pattern is the aggregation source [parent].
+   * 
+   * *Target can exist independently of the pattern.*
+   */
+  PATTERN_RELATION_AGGREGATION_SOURCE: 'PATTERN_RELATION_AGGREGATION_SOURCE',
+  /**
+   * This pattern is the aggregation target [children].
+   * 
+   * *Pattern can exist independently of the pattern.*
+   */
+  PATTERN_RELATION_AGGREGATION_TARGET: 'PATTERN_RELATION_AGGREGATION_TARGET',
 }
--- server/app/_patterns/_chain/_chain.pattern.js
+++ server/app/_patterns/_chain/_chain.pattern.js
@@ -1,6 +1,6 @@
 const { PATTERN_TYPE_SERVER } = require('../../../../_patterns/_pattern/_enums/patternType/patternType.enum')
 const {
-  PATTERN_RELATION_COMPOSITION_SOURCE,
+  PATTERN_RELATION_COMPOSITION_SPECIALIZATION_SOURCE,
   PATTERN_RELATION_COMPOSITION_TARGET
 } = require('../../../../_patterns/_pattern/_enums/patternRelation/patternRelation.enum')
 
@@ -9,21 +9,25 @@ module.exports = {
   id: '_chain',
   name: 'handler chained process',
   description: {
-    summary: 'Describes a chain of async steps in : `control`, `query`, `transform`.',
-    occurence: 'in an `.handler` file',
-    problem: ''
+    summary: 'Describes a chain of async steps (chain links) in : `controller`, `dbcall`.',
+    occurence: 'In an `.handler` file',
+    problem: 'Standardizes the server lifecycle, after the middlewares, from the handler, up to the response send.chain'
   },
   connections: {
     '_handler': {
       relation: PATTERN_RELATION_COMPOSITION_TARGET,
       number: '1'
     },
-    '_chainLink': {
-      relation: PATTERN_RELATION_COMPOSITION_SOURCE,
+    '[chainLinks]': {
+      relation: PATTERN_RELATION_COMPOSITION_SPECIALIZATION_SOURCE,
+      targets: [
+        '_controller',
+        '_dbcall'
+      ],
       count: '*'
     }
   },
-  processor: '_aggragater',
+  processor: 'chainProcessor',
   instances: {
     path: './server/app',
     key: /.chain.js/
