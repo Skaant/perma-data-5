a6a59f34535c72cd86af265b2a20e33bac2388f9
imrok le bleu <romaric.ruga@gmail.com>
2020-01-25 09:59:48 +0100

    fix server con

--- server/mongo/uriResolver/uriResolver.js
+++ server/mongo/uriResolver/uriResolver.js
@@ -1,17 +1,11 @@
-module.exports = (uri, username, password) => {
-  const usernameString = '<username>'
-  const usernameIndex = uri
-    .indexOf(usernameString)
-  const passwordString = '<password>'
-  const replaceStringLength = (usernameString
-      + ':' 
-      + passwordString)
-    .length
+module.exports = (uri, username, password) =>
 
-  return uri.slice(0, usernameIndex)
-    + username
-    + ':'
-    + password
-    + uri.slice(usernameIndex
-      + replaceStringLength)
-}
+  uri
+    .replace(
+      '<username>',
+      username
+    )
+    .replace(
+      '<password>',
+      password
+    )
