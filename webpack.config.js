d9e17f8f528eac1e7e8c74bc92f1c020db5bd31f
Romaric Ruga <romaric.ruga@univ-reims.fr>
2020-03-29 13:56:26 +0200

    travis updated for CI build

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,59 @@
-module.exports = {
-  mode: 'development',
-  // mode: 'production',
-  output: {
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "babel-loader"
-          },
-          {
-            loader: "react-svg-loader",
-            options: {
-              jsx: true
+module.exports = env => {
+
+  const {
+    mode,
+    watch
+  } = env
+    /**
+     * If `env` is not defined : 
+     *  here are set the default values. */
+    || {
+      mode: 'development',
+      watch: true
+    }
+  
+  return {
+    mode,
+    /**
+     * `env.watch` is parsed as a string when
+     *  set though the command lines, so `false`
+     *  is kinda detected here. */
+    watch: watch === true,
+    output: {
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "babel-loader"
+            },
+            {
+              loader: "react-svg-loader",
+              options: {
+                jsx: true
+              }
             }
-          }
-        ]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/react'
-            ]
-          }
-        }]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [{
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/react'
+              ]
+            }
+          }]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    }
   }
 }
